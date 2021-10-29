e41b3e717bf40000cf19d71f479cc4197a4aa901
NikoNi <bluepspower@qq.com>
2021-10-30 02:10:22 +0800

    chore: types

--- src/client/app/components/Content/index.tsx
+++ src/client/app/components/Content/index.tsx
@@ -3,13 +3,15 @@ import { MDXProvider } from '@mdx-js/react'
 import { Wrap } from './style'
 import { API, CodeBlock } from 'vitepress-rc'
 
-export const Content: FC = (props) => {
+export const Content: FC<any> = (props) => {
 	return (
 		<Wrap>
 			<MDXProvider
 				components={{
+					// @ts-ignore
 					API: API,
 					pre: (props: any) => <div {...props} />,
+					// @ts-ignore
 					code: CodeBlock,
 				}}
 			>
