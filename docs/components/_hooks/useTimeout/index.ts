73746494fa4c27e77c343c13237e718cb3fa7d60
nihao <nihao425@dingtalk.com>
2022-01-21 10:19:36 +0800

    feat: useTimeout

---
+++ docs/components/_hooks/useTimeout/index.ts
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import useLatest from "../useLatest/index";
+
+function useTimeout(
+  fn: () => void,
+  delay: number | undefined,
+): void {
+  const fnRef = useLatest(fn);
+
+  useEffect(() => {
+    if (typeof delay !== "number" || delay <= 0) {
+      return;
+    }
+
+    const timer = setTimeout(() => {
+      fnRef.current();
+    }, delay);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [delay]);
+};
+
+export default useTimeout;
